e5e4d9ae9615b2b73571be091ca6df0c56732b5f
David Waring <dev@davidwaring.net>
2019-07-15 18:44:26 -0400

    SW: Fonts - cache first

--- src/sw.js
+++ src/sw.js
@@ -2,12 +2,23 @@ workbox.skipWaiting();
 workbox.clientsClaim();
 
 workbox.routing.registerRoute(
-    new RegExp('\\.(?:js|css|woff2|png|ico)$'),
+    new RegExp('\\.(?:js|css|png|ico|json)$'),
     new workbox.strategies.StaleWhileRevalidate()
 );
 workbox.routing.registerRoute(
-    new RegExp('manifest\\.json$'),
-    new workbox.strategies.StaleWhileRevalidate()
+  new RegExp('\\.(?:woff2)$'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
 );
 
 workbox.routing.registerNavigationRoute('/');

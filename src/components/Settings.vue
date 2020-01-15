7500f7cbaca2c3d14ef4b6c7c6893eaa88e5915a
David Waring <djw64@cornell.edu>
2020-01-15 10:43:48 -0500

    [WIP] Settings: User Account

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -9,10 +9,11 @@
 
             <v-card-text class="card-content">
 
-                <v-tabs grow
+                <v-tabs grow icons-and-text
+                    v-model="activeTab"
+                    v-on:change="onTabChange"
                     height="40"
-                    color="secondary-bg"
-                    icons-and-text="icons">
+                    color="secondary-bg">
                 
                     <v-tabs-slider color="primary"></v-tabs-slider>
 
@@ -28,8 +29,7 @@
                         <v-icon>person</v-icon>
                     </v-tab>
 
-
-                    <!-- Update Settings -->
+                    <!-- SCHEDULE UPDATES -->
                     <v-tab-item class="tab-item">
                         <h3>Schedule Updates</h3>
                         <p>Check for Updates Automatically: <code>settings.updates.autoCheck = true</code></p>
@@ -37,7 +37,7 @@
                         <p>Restore Database: clear and download archive</p>
                     </v-tab-item>
 
-                    <!-- Trip Settings -->
+                    <!-- TRIP SEARCHES -->
                     <v-tab-item class="tab-item">
                         <h3>Trip Searches:</h3>
                         <p>Allow Transfers: <code>settings.search.allowTransfers = true</code></p>
@@ -49,13 +49,124 @@
                         <p>Max Transfers: <code>settings.search.maxTransfers = 2</code></p>
                     </v-tab-item>
 
-                    <!-- Account Settings -->
+                    <!-- USER ACCOUNT -->
                     <v-tab-item class="tab-item">
-                        <h3>User Accounts</h3>
-                        <p>Update Username:</p>
-                        <p>Update Email Address: Verify</p>
-                        <p>Update Password:</p>
-                        <p>Delete Account:</p>
+                        <h3>User Account</h3>
+
+                        <!-- Not Logged In -->
+                        <div v-if="!isLoggedIn">
+
+                            <p>Log In to your <strong>Right Track Account</strong></a> to update your Account settings.</p>
+
+                            <br />
+
+                            <div class="button-container">
+                                <div class="button-login">
+                                    <v-btn @click="login" color="primary">
+                                        <v-icon>person_outline</v-icon> Log In
+                                    </v-btn>
+                                </div>
+                                <div class="button-register">
+                                    <v-btn @click="register" color="primary" outline>
+                                        <v-icon>person_add</v-icon> Create Account
+                                    </v-btn>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <!-- Logged In -->
+                        <div v-if="isLoggedIn">
+                            <v-container class="px-0">
+
+                                <p><strong>Update Username:</strong></p>
+                                <v-layout row wrap>
+                                    <v-flex xs12 sm8>
+                                        <v-text-field label="Username" v-model="username" box>
+                                        </v-text-field>
+                                    </v-flex>
+                                    <v-flex sm1></v-flex>
+                                    <v-flex xs12 sm3>
+                                        <v-btn color="primary" block
+                                            @click="updateUsername"
+                                            :disabled="this.username === this.user.username">
+                                                Update
+                                        </v-btn>
+                                    </v-flex>
+                                </v-layout>
+                                
+                                <div class="hidden-sm-and-up"><br /><br /></div>
+
+                                <p><strong>Update Email Address:</strong></p>
+                                <v-layout row wrap>
+                                    <v-flex xs12 sm8>
+                                        <v-text-field label="Email Address" v-model="email" box>
+                                        </v-text-field>
+                                    </v-flex>
+                                    <v-flex sm1></v-flex>
+                                    <v-flex xs12 sm3>
+                                        <div class="email-button-container">
+                                            <div class="email-button-update">
+                                                <v-btn color="primary" block
+                                                    @click="updateEmail"
+                                                    :disabled="this.email === this.user.email">
+                                                        Update
+                                                </v-btn>
+                                            </div>
+                                            <div class="email-button-verify">
+                                                <v-btn color="primary" outline block
+                                                    @click="verifyEmail"
+                                                    :disabled="user.verified">
+                                                        <template v-if="user.verified">
+                                                            <v-icon>verified_user</v-icon>&nbsp;Verified
+                                                        </template>
+                                                        <template v-else>
+                                                            Verify
+                                                        </template>
+                                                </v-btn>
+                                            </div>
+                                        </div>
+                                    </v-flex>
+                                </v-layout>
+
+                                <div class="hidden-sm-and-up"><br /><br /></div>
+
+                                <p><strong>Update Password:</strong></p>
+                                <v-layout row wrap>
+                                    <v-flex xs12 sm8>
+                                        <v-text-field label="Current Password" v-model="password.current" box>
+                                        </v-text-field>
+                                    </v-flex>
+                                    <v-flex sm1></v-flex>
+                                    <v-flex xs12 sm3></v-flex>
+                                </v-layout>
+                                <v-layout row wrap>
+                                    <v-flex xs12 sm8>
+                                        <v-text-field label="New Password" v-model="password.new" box>
+                                        </v-text-field>
+                                    </v-flex>
+                                    <v-flex sm1></v-flex>
+                                    <v-flex xs12 sm3>
+                                        <v-btn color="primary" block
+                                            @click="updatePassword"
+                                            :disabled="!this.password.current || !this.password.new">
+                                                Update
+                                        </v-btn>
+                                    </v-flex>
+                                </v-layout>
+
+                                <div class="hidden-sm-and-up"><br /><br /></div>
+
+                                <p><strong>Delete Account:</strong></p>
+
+                                <v-btn color="error" block
+                                    @click="deleteAccount">
+                                        Delete Account
+                                </v-btn>
+
+                            </v-container>
+                        </div>
+
                     </v-tab-item>
 
                 </v-tabs>
@@ -67,7 +178,133 @@
 
 
 <script>
+    const user = require("@/utils/user.js");
+    const TAB_NAMES = ["updates", "trips", "account"];
     
+    module.exports = {
+
+        // ==== COMPONENT DATA ==== //
+        data: function() {
+            return {
+                agencyId: undefined,
+                activeTab: 0,
+                isLoggedIn: true,
+                user: {},
+                username: undefined,
+                email: undefined,
+                password: {
+                    current: undefined,
+                    new: undefined
+                }
+            }
+        },
+
+        // ==== COMPONENT FUNCTIONS ==== //
+        methods: {
+
+            /**
+             * Update the URL on tab change
+             */
+            onTabChange() {
+                this.$router.replace({
+                    name: "settings",
+                    query: {
+                        tab: TAB_NAMES[this.activeTab]
+                    }
+                });
+            },
+
+            /**
+             * Update the User's username
+             */
+            updateUsername() {
+                console.log("UPDATE USERNAME: " + this.username);
+            },
+
+            /**
+             * Update the User's email address
+             */
+            updateEmail() {
+                console.log("UPDATE EMAIL: " + this.email);
+            },
+
+            /**
+             * Verify the User's email address
+             */
+            verifyEmail() {
+                console.log("VERIFY EMAIL");
+            },
+
+            /**
+             * Update the User's password
+             * @return {[type]} [description]
+             */
+            updatePassword() {
+                console.log("UPDATE PASSWORD: " + this.password.current + " --> " + this.password.new);
+            },
+
+            /**
+             * Delete the User's account
+             * @return {[type]} [description]
+             */
+            deleteAccount() {
+                console.log("DELETE ACCOUNT");
+            },
+
+            /**
+             * Redirect to the Login page
+             */
+            login() {
+                this.$router.push({
+                    name: "login",
+                    query: {
+                        agency: this.agencyId,
+                        src: '/' + this.agencyId + '/settings?tab=account'
+                    }
+                });
+            },
+
+            /**
+             * Redirect to the Registration page
+             */
+            register() {
+                this.$router.push({
+                    name: "register",
+                    query: {
+                        agency: this.agencyId,
+                        src: '/' + this.agencyId + '/settings?tab=account'
+                    }
+                });
+            },
+
+        },
+
+        // ==== COMPONTENT MOUNTED ==== //
+        mounted() {
+            let vm = this;
+            vm.agencyId = vm.$route.params.agency;
+            
+            // Set Active Tab
+            let tab = vm.$route.query.tab;
+            for ( let i = 0; i < TAB_NAMES.length; i++ ) {
+                if ( TAB_NAMES[i] === tab ) {
+                    vm.activeTab = i;
+                }
+            }
+
+            // Set Logged In Status
+            user.isLoggedIn(function(isLoggedIn, user) {
+                vm.isLoggedIn = isLoggedIn;
+                if ( isLoggedIn ) {
+                    vm.user = user;
+                    vm.username = user.username;
+                    vm.email = user.email;
+                }
+            });
+        }
+
+    }
+
 </script>
 
 
@@ -80,10 +317,45 @@
     .tab-item {
         padding: 16px;
     }
-    .tab-item > h3 {
+    .tab-item h3 {
         border-bottom: 1px solid #999;
     }
     .tab-item p {
         margin: 10px 5px;
     }
+
+    .button-container {
+        display: grid;
+        grid-gap: 10px;
+        grid-template-columns: 1fr;
+        grid-template-areas: "login" "register";
+    }
+    @media (min-width: 600px) {
+        .button-container {
+            grid-template-columns: 1fr 1fr;
+            grid-template-areas: "register login";
+        }
+    }
+    .button-login {
+        grid-area: login;
+        text-align: center;
+    }
+    .button-register {
+        grid-area: register;
+        text-align: center;
+    }
+    .button-container .v-btn {
+        width: 90%;
+        max-width: 300px;
+    }
+
+    .email-button-container {
+        margin-top: 0px;
+    }
+    @media (min-width: 600px) {
+        .email-button-container {
+            margin-top: -15px;
+        }
+    }
+
 </style>
